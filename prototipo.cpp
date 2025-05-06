59f83cc1ee67378e05536623391d2df07f687b5c
whoisleoo <leoaaa96@gmail.com>
2025-05-06 09:08:21 -0500

    prototipo.cpp

--- prototipo.cpp
+++ prototipo.cpp
@@ -12,23 +12,36 @@ struct Produto
     vector<string> product;
 };
 
-Produto produtos;
+struct ItemCarrinho
+{
+    int indiceProduto;
+    int quantProduto;
+};
 
+
+Produto produtos;
+vector<ItemCarrinho> carrinho;
 
 void registrarProduto();
 void listaProduto();
 void realizarVenda();
+void carrinhoCompras();
+void visualizarCarrinho();
+void finalizarCompra();
 void clearScreen();
 
 
 int main(){
     do{
-    cout << "Bem vindo ao sistema de super-mercado VERSÃO BETA 1.0\n\n";
+    cout << "Bem vindo ao sistema de super-mercado VERSÃO BETA 2.0\n\n";
     cout << "[SELECIONE UMA OPÇÃO]\n";
     cout << "[1] Registrar produto.\n";
     cout << "[2] Lista de produtos.\n";
     cout << "[3] Realizar venda.\n";
-    cout << "[4] Sair.\n";
+    cout << "[4] Carrinho de Compras.\n";
+    cout << "[5] Visualizar Carrinho.\n";
+    cout << "[6] Finalizar Compras. \n";
+    cout << "[7] Sair.\n";
     cin >> mainOption;
 
     switch (mainOption)
@@ -43,10 +56,20 @@ int main(){
         realizarVenda();
         break;
     case 4:
+        carrinhoCompras();
+        break;
+    case 5:
+        visualizarCarrinho();
+        break;
+    case 6:
+        finalizarCompra();
+        break;
+    case 7:
         close = false;
         break;
     default:
-
+        cout << "Invalido.\n";
+        clearScreen();
         break;
     }
 
@@ -81,6 +104,8 @@ void registrarProduto(){
 void listaProduto(){
     clearScreen();
     cout << "[LISTA DE PRODUTOS]\n\n";
+    cout << "EM ESTOQUE: " << produtos.product.size() << "\n\n";
+
     for(int i = 0; i < produtos.product.size(); i++){
             cout << " | NUMERAÇÃO: " << i << "\n";
             cout << " | PRODUTO: " << produtos.product[i] << "\n";
@@ -97,37 +122,48 @@ void realizarVenda(){
     do{
     clearScreen();
     int num;
+    int quantidadeProduto;
     float valorPago;
     cout << "[REALIZAR VENDA DE PRODUTO]\n";
     cout << "Digite a numeração do produto a ser comprado (Caso não saiba, verifica a lista de produtos) : ";
     cin >> num;
+    cout << "Digite a quantidade do produto: ";
+    cin >> quantidadeProduto;
+    if(quantidadeProduto > 0){
 
     clearScreen();
+    float valorTotal = produtos.price[num] * quantidadeProduto;
 
     if(num >= 0 && num < produtos.product.size()){
     cout << "[SUCESSO]\n\n";
-    cout << "| PRODUTO ENCONTRADO: " << produtos.product[num] << " | PREÇO: R$" << produtos.price[num] << " REAIS | \n\n";
+    cout << "| PRODUTO ENCONTRADO: " << produtos.product[num] << " | QUANTIDADE: " << quantidadeProduto << "\n";
+    cout << "| PREÇO TOTAL: R$" << valorTotal << " REAIS | \n\n";
     cout << "Digite o valor pago pelo cliente: R$";
     cin >> valorPago;
     clearScreen();
-        if(valorPago == produtos.price[num]){
+
+
+        if(valorPago == valorTotal){
             cout << "Produto pago com sucesso!\n\n";
-            cout << "Valor do produto: R$" << produtos.price[num] << " Reais \n";
+            cout << "Valor do produto: R$" << valorTotal << " Reais \n";
             cout << "Valor pago pelo cliente: R$" << valorPago << " Reais \n";
             cout << "TOTAL: R$" << valorPago << " Reais \n";
-        }else if(valorPago < produtos.price[num]){
-            float valorNecessario = produtos.price[num] - valorPago;
+            closeSale = false;
+        }else if(valorPago < valorTotal){
+            float valorNecessario = valorTotal - valorPago;
             cout << "Não foi possivel realizar o pagamento!\n\n";
-            cout << "Valor do produto: R$" << produtos.price[num] << " Reais \n";
+            cout << "Valor do produto: R$" << valorTotal << " Reais \n";
             cout << "Valor pago pelo cliente: R$" << valorPago << " Reais \n\n";
             cout << "Ainda precisa de: R$" << valorNecessario << " Reais para completar o pagamento\n";
+            closeSale = false;
 
-        }else if(valorPago > produtos.price[num]){
-            float troco = valorPago - produtos.price[num];
+        }else if(valorPago > valorTotal){
+            float troco = valorPago - valorTotal;
             cout << "Produto pago com sucesso!\n\n";
-            cout << "Valor do produto: R$" << produtos.price[num] << " Reais \n";
+            cout << "Valor do produto: R$" << valorTotal << " Reais \n";
             cout << "Valor pago pelo cliente: R$" << valorPago << " Reais \n\n";
             cout << "Troco do cliente: R$" << troco << " Reais \n";
+            closeSale = false;
         } else{
             cout << "ERRO [404].\n";
             cout << "Algo deu errado, verifique se tudo foi digitado corretamente e tente novamente.\n\n";
@@ -137,11 +173,135 @@ void realizarVenda(){
         cout << "NÃO FOI ENCONTRADO NENHUM PRODUTO VÁLIDO COM ESSA NUMERAÇÃO!\n";
         cout << "(Verifique a lista de produtos pra uma consulta mais avançada)\n\n";
     }
+}else{
+    cout << "[ERRO]\n\n";
+    cout << "Quantidade de produto inválida.\n";
+}
     cout << "Pressione qualquer tecla para continuar... ";
     cin.ignore();
     cin.get();
+    clearScreen();
  } while(closeSale);
 }
+
+
+
+
+void carrinhoCompras() {
+    int numProduto;
+    int quantidade;
+
+    clearScreen(); 
+    cout << "Digite o número do produto a ser adicionado no carrinho: ";
+    cin >> numProduto;
+
+    if (numProduto >= 0 && numProduto < produtos.product.size()) {
+        cout << "Produto selecionado: " << produtos.product[numProduto] << "\n\n";
+        
+        cout << "Digite a quantidade a ser comprada: ";
+        cin >> quantidade;
+
+        bool numEncontrado = false;
+        
+    
+        for (auto& item : carrinho) {
+            if (item.indiceProduto == numProduto) {
+               
+                item.quantProduto += quantidade;
+                numEncontrado = true;
+                break;
+            }
+        }
+
+        if (!numEncontrado) {
+            carrinho.push_back({numProduto, quantidade});
+            cout << "Produto(s) adicionado(s) com sucesso ao carrinho!\n\n";
+        } else {
+            cout << "Produto atualizado no carrinho!\n\n";
+        }
+
+    } else {
+        cout << "[ERRO 404]\n\n";
+        cout << "NÃO FOI ENCONTRADO NENHUM PRODUTO VÁLIDO COM ESSA NUMERAÇÃO!\n";
+        cout << "(Verifique a lista de produtos para uma consulta mais avançada)\n\n";
+    }
+
+    cout << "Pressione qualquer tecla para continuar... ";
+    cin.ignore();
+    cin.get();
+    clearScreen();
+}
+
+
+void visualizarCarrinho(){
+clearScreen();
+cout << "[Carrinho de Compras]\n\n";
+float totalCarrinho = 0.0;
+
+for(auto& item : carrinho){
+    float precoProduto = produtos.price[item.indiceProduto];
+    string nomeItem = produtos.product[item.indiceProduto];
+    float totalItem = precoProduto * item.quantProduto;
+
+    cout << "Produto: " << nomeItem << " | Quantidade: " << item.quantProduto << "\n";
+    cout << "Preço: R$" << precoProduto << "\n";
+    cout << "Total: R$" << totalItem << "\n\n";
+
+    totalCarrinho += totalItem;
+}
+
+cout << "\n Total do carrinho: R$" << totalCarrinho << "\n\n";
+cout << "Pressione qualquer tecla para continuar... ";
+cin.ignore();
+cin.get();
+clearScreen();
+}
+
+
+
+void finalizarCompra(){
+clearScreen();
+float totalCarrinho = 0.0;
+
+for(auto& item : carrinho){
+    totalCarrinho += produtos.price[item.indiceProduto] * item.quantProduto;
+}
+
+if(totalCarrinho > 0){
+
+cout << "[FINALIZAR COMPRA]\n\n";
+cout << "Valor total da compra: R$" << totalCarrinho << "\n\n";
+
+float valorPago;
+cout << "Valor pago pelo cliente: R$";
+cin >> valorPago;
+
+if(valorPago < totalCarrinho){
+    float valorFaltante;
+    cout << "Valor insuficiente para concluir a compra.\n\n";
+    valorFaltante = totalCarrinho - valorPago;
+    cout << "Ainda falta: " << valorFaltante << "\n";
+} else{
+    float troco = valorPago - totalCarrinho;
+    cout << "Compra concluida com sucesso!\n\n";
+    cout << "Troco do cliente: R$" << troco << "\n";
+
+    carrinho.clear();
+    cout << "Pressione qualquer tecla para continuar... ";
+    cin.ignore();
+    cin.get();
+    clearScreen();
+}
+
+} else{
+cout << "Seu carrinho está vazio, pressione qualquer tecla pra  continuar... ";
+    cin.ignore();
+    cin.get();  
+    clearScreen();
+    }
+}
+
+
 
 void clearScreen() {
     string escape = "\033[2J\033[1;1H";
