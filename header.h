92459a819464e088d09d3bd51aeccf5bb4ad2652
whoisleoo <leoaaa96@gmail.com>
2025-05-16 15:47:37 -0300

    Add files via upload

---
+++ header.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+#include <iomanip> 
+#include <thread>  
+#include <chrono>
+#include <string>
+#include <vector>
+#include <windows.h>
+using namespace std;
+using json = nlohmann::json;
